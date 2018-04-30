931ff6c85cb408b1a7aed4f9a96d634b8400c0a2
rgossomjr@gmail.com <rgossomjr@gmail.com>
2018-04-30 06:39:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function logWhisper(string){
 console.log ("hello")
 }
 
-function sayHiToGrandma("I can\'t hear you!"){
+function sayHiToGrandma(string){
   return string.toLowerCase("I can\'t hear you!")
 }
